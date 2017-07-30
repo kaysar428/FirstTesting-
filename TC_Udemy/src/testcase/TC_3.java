ccad9e02f5d2c8feb0752d0ab6da875e710befb0
kaiser <kaysar1328@gmail.com>
2017-07-30 14:45:28 -0400

    this is udemy practice

---
+++ TC_Udemy/src/testcase/TC_3.java
@@ -0,0 +1,22 @@
+package testcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_3 {
+
+	
+	@Test
+	
+	   public void testcase1(){
+		
+		System.setProperty("webdriver.chrome.driver","./src/driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.findElement(By.id("lst-ib")).sendKeys("Realmadrid");
+		driver.findElement(By.cssSelector("input[name=btnK]")).click();
+		driver.quit();
+	}
+	
+}
--- phptravels.net/src/test/java/phptravels/net/cukerunner/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Kaysa
- *
- */
-package phptravels.net.cukerunner;
